ca210d273c49f16e78c53d2cd33827c2dce78ddc
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-10-10 01:23:20 -0300

    add

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9u9M1kSHFFHTOSh791tXwkHjg5DtaIz4",
-  authDomain: "aapp-59a50.firebaseapp.com",
-  projectId: "aapp-59a50",
-  storageBucket: "aapp-59a50.appspot.com",
-  messagingSenderId: "591028621760",
-  appId: "1:591028621760:web:cc9f19a329b82113987825",
-  measurementId: "G-M6JSFWPDHX"
+ 
 };
 
 // Initialize Firebase
